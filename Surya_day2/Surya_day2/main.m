2665fb2a88ffd4df8b7d487c7c0d3b8fa2cbb2ef
jaisimhareddy <jaisimhareddy.surya@gmail.com>
2014-05-06 14:09:15 -0400

    Day2Starter
    Day2 class of objective c

---
+++ Surya_day2/Surya_day2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Surya_day2
+//
+//  Created by macadmin on 2014-05-06.
+//  Copyright (c) 2014 ObjC-day2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
