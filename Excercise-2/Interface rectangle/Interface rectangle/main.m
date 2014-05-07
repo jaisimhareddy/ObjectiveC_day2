f94edd84f9a52b75fae0fd27949aa580f9ed6098
Surya <jaisimhareddy.surya@gmail.com>
2014-05-07 17:08:12 -0400

    Excercise-2 assignment

--- Excercise-2/Interface rectangle/Interface rectangle/main.m
+++ Excercise-2/Interface rectangle/Interface rectangle/main.m
@@ -7,32 +7,80 @@
 //
 
 #import <Foundation/Foundation.h>
-@interface rectangle:NSObject
--(void) setWidth: (int) w;
--(void) setHeight: (int) h;
--(int) width;
--(int) height;
+@interface Rectangle : NSObject
+-(void) setWidth : (int) w;
+-(void) setHeight : (int) h;
+-(int) getWidth;
+-(int) getHeight;
 -(int) area;
 -(int) perimeter;
 @end
-@implementation rectangle
+@implementation Rectangle
+
 {
-    void rectangle;
+int width, height, area, perimeter;
+    
+}
+-(void) setWidth:(int)w
+
+{
+    
+    width = w;
     
 }
 
-<#methods#>
+-(void) setHeight:(int)h
 
-@end
-
-int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        
-        
-        
-    }
-    return 0;
+    
+    height = h;
+    
 }
 
+-(int) getHeight
+
+{
+    
+    return height;
+    
+}
+-(int) getWidth
+
+{
+    return width;
+    
+}
+-(int) area
+{
+area= width*height;
+    
+    return area;
+    
+}
+-(int) perimeter
+
+{
+perimeter= 2*(width+height);
+    
+    return perimeter;
+}
+@end
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        
+        Rectangle *newrectangle = [[Rectangle alloc] init];
+        
+        [newrectangle setHeight:300];
+        
+        [newrectangle setWidth:400];
+        
+        NSLog(@"\nArea of Ractangle is %i", [newrectangle area]);
+        
+        NSLog(@"\nPerimeter of Ractangle is %i", [newrectangle perimeter]);
+        
+        }
+    
+    return 0;
+    
+}
