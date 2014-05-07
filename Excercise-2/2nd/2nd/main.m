06067def723aa1d94d5d542a24e88b20a707394b
Surya <jaisimhareddy.surya@gmail.com>
2014-05-07 16:22:39 -0400

    Exercise

---
+++ Excercise-2/2nd/2nd/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  2nd
+//
+//  Created by user2 on 2014-05-07.
+//  Copyright (c) 2014 Surya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        char c,d;
+        c='d';
+        d=c;
+        NSLog(@"d=%c",d);
+    }
+    return 0;
+}
+
