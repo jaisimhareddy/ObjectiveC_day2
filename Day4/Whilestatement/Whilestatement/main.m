3bd484c3b64e59868b967e67285f84026a830173
Surya <jaisimhareddy.surya@gmail.com>
2014-05-08 15:14:18 -0400

    WhileLoopstatement
    While loop condition code

---
+++ Day4/Whilestatement/Whilestatement/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  Whilestatement
+//
+//  Created by user2 on 2014-05-08.
+//  Copyright (c) 2014 Surya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int count = 1;
+        while (count <= 5) {
+            NSLog(@"%i", count);
+            ++count;
+            
+        }
+        
+    }
+    return 0;
+}
+
