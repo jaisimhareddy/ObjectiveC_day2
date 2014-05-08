cfdc1bb534870d1ae7022f8dbe513c3cde5ad5f6
Surya <jaisimhareddy.surya@gmail.com>
2014-05-08 16:56:53 -0400

    Day4Assignments-1st
    1st assignment

---
+++ Day4/Excercisesday4/1st/1st/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  1st
+//
+//  Created by user2 on 2014-05-08.
+//  Copyright (c) 2014 Surya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int n;
+        int fact=1;
+        for(n=1;n <=10 ;++n)
+            fact *= n;
+        NSLog(@"%i", fact);
+            
+        
+    }
+    return 0;
+}
+
