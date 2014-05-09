54065086991ec19a30f92bfcf5e9b5e2fa0ae0a1
Surya <jaisimhareddy.surya@gmail.com>
2014-05-09 12:52:34 -0400

    Day4-Exercise2
    assignment-2

---
+++ Day4/Excercisesday4/2ndassignment/2ndassignment/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  2ndassignment
+//
+//  Created by user2 on 2014-05-08.
+//  Copyright (c) 2014 Surya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int n, n2, i;
+        for()
+        
+    }
+    return 0;
+}
+
