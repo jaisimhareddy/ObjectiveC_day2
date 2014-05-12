5a3ae42417777c9e40c5451e415766e6060ebde4
Surya <jaisimhareddy.surya@gmail.com>
2014-05-12 14:57:30 -0400

    Initial Commit

---
+++ Day4/Excercisesday4/Exercise5/Exercise5/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Exercise5
+//
+//  Created by user2 on 2014-05-12.
+//  Copyright (c) 2014 Lambton College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
