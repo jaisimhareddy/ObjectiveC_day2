18e58cd4998c45d0bbec3a2f7555ba298149a562
Surya <jaisimhareddy.surya@gmail.com>
2014-05-12 13:37:14 -0400

    Leapyearcode

--- Day6/Remindercode/Remindercode/main.m
+++ Day6/Remindercode/Remindercode/main.m
@@ -13,8 +13,15 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int year, rem_4, rem_100, rem_400;
+        NSLog(@"Enter the year to be tested:");
+        scanf("%i", &year);
+        rem_4= year %4;
+        rem_100= year %100;
+        rem_400= year % 400;
+         if ((rem_4==0 && rem_100 !=0) || rem_400 ==0)
+             NSLog(@"It's a leap year.");
+         else NSLog(@"Nope , it's not a leap year.");
         
     }
     return 0;
