9d1df0e04d36d52c5d115ef0c123e33cbdf4905e
Surya <jaisimhareddy.surya@gmail.com>
2014-05-12 14:02:09 -0400

    SignFunction

--- Day6/SignfunctionCode/SignfunctionCode/main.m
+++ Day6/SignfunctionCode/SignfunctionCode/main.m
@@ -12,9 +12,18 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int number, sign;
+        NSLog(@"Please type the number");
+        scanf("%i", &number);
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if (number<0)
+            sign =-1;
+        else if (number==0)
+            sign = 0;
+        else //must be positive
+            sign =1;
+        NSLog(@"sign =%i", sign);
+        
         
     }
     return 0;
