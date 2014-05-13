8e1d629ef295942a3702487ae7ab576ebae5820d
Surya <jaisimhareddy.surya@gmail.com>
2014-05-13 13:57:35 -0400

    Initial Commit

---
+++ Day7/Primecode/Primecode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Primecode
+//
+//  Created by user2 on 2014-05-13.
+//  Copyright (c) 2014 Lambton College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
