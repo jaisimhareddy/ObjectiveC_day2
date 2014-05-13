b893e5bdfe11f1e02d5e6dbdb0488748a265fe5b
Surya <jaisimhareddy.surya@gmail.com>
2014-05-13 13:20:56 -0400

    Alphabeticcode

--- Day7/Alphabeticcode/Alphabeticcode/main.m
+++ Day7/Alphabeticcode/Alphabeticcode/main.m
@@ -12,10 +12,16 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        char c;
+        NSLog(@"enter a single character:");
+        scanf("%c", &c);
+        if ((c >='a' && c <='z') || (c >='A' && c<='Z')) {
+            NSLog(@"It's an alphabetic character.");
+        }else if (c >='0' && c<='9') {
+            NSLog(@"It's a digit.");
+        } else {
+            NSLog(@"It's a special character");
+        }
     }
     return 0;
 }
