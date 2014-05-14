b983441e883c7f802a42c4d8bf2906c8bee89c69
Surya <jaisimhareddy.surya@gmail.com>
2014-05-14 13:45:28 -0400

    Initial Commit

---
+++ Day4/Excercisesday4/8thAssignment/8thAssignment/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  8thAssignment
+//
+//  Created by user2 on 2014-05-14.
+//  Copyright (c) 2014 Lambton College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
