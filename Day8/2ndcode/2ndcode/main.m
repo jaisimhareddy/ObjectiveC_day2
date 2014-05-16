d987ebabf9f1bba8fb72648481b4e2dbbaec312e
Surya <jaisimhareddy.surya@gmail.com>
2014-05-16 14:03:19 -0400

    Day8-Boolcode

---
+++ Day8/2ndcode/2ndcode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  2ndcode
+//
+//  Created by user2 on 2014-05-16.
+//  Copyright (c) 2014 Lambton College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code her
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
