d96dbb94772cf8262a698665be509cee7b87554e
Surya <jaisimhareddy.surya@gmail.com>
2014-05-16 13:55:32 -0400

    Day8-Boolcode

--- Day8/Day8code/Day8code/main.m
+++ Day8/Day8code/Day8code/main.m
@@ -13,12 +13,22 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         int p, d; BOOL isPrime;
-        for ( p = 2; p <= 50; ++p ) { isPrime = YES;
-            for ( d = 2; d < p; ++d ) if ( p % d == 0 )
+        for ( p = 2; p <= 50; ++p )
+        {
+            isPrime = YES;
+            for ( d = 2; d < p; ++d )
+            {
+                
+            if ( p % d == 0 )
+            {
                 isPrime = NO;
-            if ( isPrime == YES ) NSLog (@"%i ", p);
+            }
+            }
+            if ( isPrime == YES )
+                NSLog (@"%i ", p);
         }
     }
+    
     return 0;
 }
 
